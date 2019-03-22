358aec82fc99618ac32825187198b74758d0d690
Pradyumna <vinjumup@timeinc.com>
2019-03-23 03:25:11 +0530

    Initial Commit

---
+++ payu-angular-client/src/app/payment/payment.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css']
+})
+export class PaymentComponent implements OnInit {
+
+  public payuform: any = {};
+  disablePaymentButton: boolean = true;
+  constructor(private http: HttpClient) { }
+  confirmPayment() {
+    const paymentPayload = {
+      email: this.payuform.email,
+      name: this.payuform.firstname,
+      phone: this.payuform.phone,
+      productInfo: this.payuform.productinfo,
+      amount: this.payuform.amount
+    }
+    return this.http.post<any>('http://localhost:8080/payment/payment-details', paymentPayload).subscribe(
+      data => {
+      console.log(data);
+      this.payuform.txnid = data.txnId;
+      this.payuform.surl = data.sUrl;
+      this.payuform.furl = data.fUrl;
+      this.payuform.key = data.key;
+      this.payuform.hash = data.hash;
+      this.payuform.txnid = data.txnId;
+        this.disablePaymentButton = false;
+    }, error1 => {
+        console.log(error1);
+      })
+  }
+
+  ngOnInit() {
+  }
+
+}
